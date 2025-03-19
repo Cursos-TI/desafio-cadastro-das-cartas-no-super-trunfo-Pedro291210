f31ce65037b148717ab9d6d8f9aac5cfb920aa42
Pedro291210 <inspedro@hotmail.com>
2025-03-19 01:43:53 +0000

    cadastrei duas cartas do desafio

---
+++ cadastro carta2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+ 
+// Desafio Super Trunfo
+
+printf ("Carta: 02\n");
+
+    // Declaração de variaveis
+
+char Estado = 'R';
+
+char Codigodacarta[30] = "R01";
+
+char Cidade[50] = "Rio de janeiro";
+
+int Populacao = 6748000;
+
+float Area = 1200.25;
+
+float Pib = 300.50;
+
+int pontoturisticos = 30;
+
+// Imprimindo valores das variaveis
+
+
+printf("Estado: %c \n", Estado);
+
+printf("Código: %s\n", Codigodacarta);
+
+printf("Nome da Cidade: %s\n", Cidade);
+
+printf("População: %d\n", Populacao);
+
+printf("Área: %f\n", Area);
+
+printf("Pib: %f\n", Pib);
+
+printf("Números de Pontos Turisticos: %d\n", pontoturisticos);
+
+return 0;
+
+
+
+
+
+
+}
