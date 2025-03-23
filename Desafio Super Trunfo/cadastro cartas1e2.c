ea39feebcd09e89786945a53df01eb407be5b8b3
Pedro291210 <inspedro@hotmail.com>
2025-03-23 03:32:30 +0000

    Novo commit

---
+++ Desafio Super Trunfo/cadastro cartas1e2.c
@@ -0,0 +1,85 @@
+# include <stdio.h>
+int main (){
+
+//Desafio Super Trunfo
+
+printf ("Carta: 01\n");
+
+//Declarando variaveis
+
+char Estado1 = 'S';
+
+char Codigodacarta1[40] = "S01";
+
+char Cidade1[30] = "São Paulo";
+
+int Populacao1 = 10748000;
+
+float Area1 = 20200.25;
+
+float Pib1 = 3300.50;
+
+int pontoturisticos1 = 60;
+
+
+// Declaração das variaveis da Carta 02
+
+char Estado2 = 'R';
+
+char Codigodacarta2[30] = "R01";
+
+char Cidade2[50] = "Rio de janeiro";
+
+int Populacao2 = 6748000;
+
+float Area2 = 1200.25;
+
+float Pib2 = 300.50;
+
+int pontoturisticos2 = 30;
+
+
+
+// Imprimindo valores das variaveis da Carta 01
+
+
+printf("Estado: %c \n", Estado1);
+
+printf("Código: %s\n", Codigodacarta1);
+
+printf("Nome da Cidade: %s\n", Cidade1);
+
+printf("População: %d\n", Populacao1);
+
+printf("Área: %f\n", Area1);
+
+printf("Pib: %f\n", Pib1);
+
+printf("Números de Pontos Turisticos: %d\n", pontoturisticos1);
+
+
+// Imprimindo valores das variaveis da Carta 02
+
+printf ("Carta 02\n");
+
+printf("Estado: %c \n", Estado2);
+
+printf("Código: %s\n", Codigodacarta2);
+
+printf("Nome da Cidade: %s\n", Cidade2);
+
+printf("População: %d\n", Populacao2);
+
+printf("Área: %f\n", Area2);
+
+printf("Pib: %f\n", Pib2);
+
+printf("Números de Pontos Turisticos: %d\n", pontoturisticos2);
+
+
+
+
+return 0;
+
+
+}
