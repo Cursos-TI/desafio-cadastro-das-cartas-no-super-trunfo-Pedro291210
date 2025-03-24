95010c99c07741da07b401cc9960ecb257d530c9
Pedro291210 <inspedro@hotmail.com>
2025-03-24 14:47:06 +0000

    CARTA 1

--- cadastro carta2.c
+++ cadastro carta2.c
@@ -6,37 +6,37 @@ int main (){
 
     //Declarando variaveis
 
-char Estado = 'R';
+char Estado2 = 'R';
 
-char Codigodacarta[30] = "R01";
+char Codigodacarta2[30] = "R01";
 
-char Cidade[50] = "Rio de janeiro";
+char Cidade2[50] = "Rio de janeiro";
 
-int Populacao = 6748000;
+int Populacao2 = 6748000;
 
-float Area = 1200.25;
+float Area2 = 1200.25;
 
-float Pib = 300.50;
-int pontoturisticos = 30;
+float Pib2 = 300.50;
+int pontoturisticos2 = 30;
 
 
 // Imprimindo valores das variaveis da Carta 02
 
 printf ("Carta 02\n");
 
-printf("Estado: %c \n", Estado);
+printf("Estado: %c \n", Estado2);
 
-printf("Código: %s\n", Codigodacarta);
+printf("Código: %s\n", Codigodacarta2);
 
-printf("Nome da Cidade: %s\n", Cidade);
+printf("Nome da Cidade: %s\n", Cidade2);
 
-printf("População: %d\n", Populacao);
+printf("População: %d\n", Populacao2);
 
-printf("Área: %f\n", Area);
+printf("Área: %f\n", Area2);
 
-printf("Pib: %f\n", Pib);
+printf("Pib: %f\n", Pib2);
 
-printf("Números de Pontos Turisticos: %d\n", pontoturisticos);
+printf("Números de Pontos Turisticos: %d\n", pontoturisticos2);
 
 
 
